dd3e9e6b61f75ec4888965139b057e98f9611a81
straufer <paungcuy15@gmail.com>
2023-10-07 13:46:38 +0700

    link fixed

--- src/pages/home/App.vue
+++ src/pages/home/App.vue
@@ -3,6 +3,6 @@
       <h1 class="text-5xl text-center p-5">Github Repo</h1>
     </div>
     <div class="bg-fuchsia-200">
-      <p class="text-5xl text-gray-600 text-center p-4 underline"><a href="https://github.com/straufer/demo-pwa">github.com/straufer/demo-pwa</a></p>
+      <p class="text-5xl text-gray-600 text-center p-4 underline"><a href="https://github.com/straufer/demo-pwa" target="_blank">github.com/straufer/demo-pwa</a></p>
     </div>
 </template>
