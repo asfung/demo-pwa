ef9b4447f5233d3e948ca7756b9f24b6a3f4892a
straufer <paungcuy15@gmail.com>
2023-10-13 15:15:29 +0700

    add components

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer>
+        <h1>ini adalah footer</h1>
+    </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+
+import { NuxtLink } from '#build/components';
+<template>
+    <header>
+        <h1>Perusahaan</h1>
+        <NuxtLink>keren</NuxtLink>
+        <NuxtLink>keren</NuxtLink>
+        <NuxtLink>keren</NuxtLink>
+        <NuxtLink>keren</NuxtLink>
+    </header>
+</template>
