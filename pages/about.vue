5cc0c8788fd1b0327f22e89dd338a1a66dfa87e4
straufer <paungcuy15@gmail.com>
2023-10-15 17:33:01 +0700

    home and about update

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import Header from '../components/Header.vue'
+    import Footer from '../components/Footer.vue'
+    document.title = 'About'
+</script>
+
+<template>
+    <div class="header">
+        <Header />
+    </div>
+    <div class="content mt-3 ml-64">
+        <div class="row w-3/5 text-justify">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae eligendi voluptatibus quo ullam amet cupiditate ea rem consectetur dicta quod laudantium aspernatur animi, nulla, ipsa laboriosam rerum! Dolores consequatur alias, sunt tenetur ipsum tempora optio dolore quo animi inventore nobis earum, doloribus maiores sed saepe similique, veritatis quaerat non!</p>
+        </div>
+    </div>
+    <div class="footer">
+        <Footer />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
 <script setup>
-    import NavbarAdikADik from '../components/Header.vue'
+    import Header from '../components/Header.vue'
+    import Footer from '../components/Footer.vue'
+    document.title = 'Home'
 </script>
 
 <template>
-    <NavbarAdikADik />
-    <h1 class="text-5xl">ini halaman home / index</h1>
+    <div class="header">
+        <Header />
+    </div>
+    <div class="content">
+        <div class="row flex ml-80 mt-6">
+            <h1 class="text-5xl text-blue-950">Lorem Ipsum.</h1> 
+        </div>
+        <div class="row flex ml-80 mt-6">
+            <h1 class="text-2xl text-grey-950 text-justify w-96">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque obcaecati rem odit laudantium! Sapiente, sed quidem soluta non id ea.</h1> 
+        </div>
+    </div>
+    <div class="footer">
+        <Footer />
+    </div>
 </template>
