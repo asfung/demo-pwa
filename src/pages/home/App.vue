c91522d5011d63595e3a1fdc055da83ca7e0b680
straufer <paungcuy15@gmail.com>
2023-10-15 15:53:01 +0700

    temoved

--- src/pages/home/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="bg-green-500">
-      <h1 class="text-5xl text-center p-5">Github Repo</h1>
-    </div>
-    <div class="bg-fuchsia-200">
-      <p class="text-5xl text-gray-600 text-center p-4 underline"><a href="https://github.com/straufer/demo-pwa" target="_blank">github.com/straufer/demo-pwa</a></p>
-    </div>
-</template>
