ee430d717a1dcecd6c443183ab8c381fbecd553c
straufer <paungcuy15@gmail.com>
2023-10-19 10:24:21 +0700

    update cart pages

---
+++ pages/cart.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import Header from '../components/Header.vue'
+    import Footer from '../components/Footer.vue'
+    document.title = "Cart"
+</script>
+
+<template>
+    <div class="header">
+        <Header />
+    </div>
+    <div class="content">
+        <div>
+            <h1 class="text-4xl">Ini Halaman Cart</h1>
+        </div>
+    </div>
+    <div class="footer">
+        <Footer />
+    </div>
+</template>
