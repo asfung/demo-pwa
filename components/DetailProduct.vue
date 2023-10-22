91b8d00eb05a294e3fcfeeab76d3450a965e8ec0
straufer <paungcuy15@gmail.com>
2023-10-22 17:41:50 +0700

    adding card component

---
+++ components/DetailProduct.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+const props = defineProps({
+  product: {
+    type: Object,
+    default: {},
+  },
+});
+
+
+function geohot(){
+  if (props.product.id === 4) {
+    return "u cant buy it!!";
+  }else{
+    return "Add to Card"
+  }
+}
+
+const addCartStyle = computed(() => {
+  const styles = {};
+
+  if (props.product.id === 4) {
+    // just dont care to the code below
+    styles.color = 'white';
+    styles.fontWeight = 'bold';
+    styles.backgroundColor = 'red';
+    styles.cursor = 'not-allowed';
+  }
+
+  return styles;
+});
+
+</script>
+
+<template>
+  <section class="py-10">
+    <div class="container">
+      <NuxtLink
+        to="/product"
+        class="bg-white border border-slate-300 w-max flex items-center gap-1 py-2 px-5 rounded-full mb-7 cursor-pointer">
+        <i class="ri-arrow-left-s-line text-base font-medium"></i>
+        <span class="text-base font-medium">Kembali</span>
+      </NuxtLink>
+      <div class="flex items-center">
+        <div
+          class="w-1/2 bg-gray-300 mr-5 rounded-3xl flex justify-center items- center p-5 h-[500px]">
+          <img
+            :src="props.product.image"
+            class="w-full h-full object-contain"/>
+        </div>
+        <div class="w-1/2 pl-5">
+          <p class="text-xl font-light mb-3">{{ props.product.category }}</p>
+          <h1 class="text-4xl font-bold mb-3">{{ props.product.name }}</h1>
+          <h3 class="text-4xl font-light mb-3">${{ props.product.price }}</h3>
+          <p class="mb-10">{{ props.product.description }}</p>
+          <div class="flex flex-col gap-4">
+            <div
+              class="w-full flex items-center gap-2 bg-blue-600 text-white py- 3 justify-center rounded-lg cursor-pointer hover:bg-blue-600/80 transition duration-300 relative h-11 font-bold"
+              :style="addCartStyle"
+              >
+              <i class="ri-shopping-cart-2-line"></i>
+              <span>{{ geohot() }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ components/cards/CardProduct.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+  const props = defineProps({
+    product: {
+      type: Object,
+      default: {},
+    }
+  });
+</script>
+
+<template>
+    <section class="bg-white shadow-xl rounded-xl overflow-hidden">
+        <div :class="`w-full h-[200px] p-5 bg-gray-300`">
+            <img :src="props.product.image" class="w-full h-full object-contain h-auto w-auto"/>
+        </div>
+        <div class="px-5 pb-5 pt-9 relative">
+            <NuxtLink :to="`/product/${props.product.id}`" ><h3 class="text-lg font-
+            bold mb-4 text-limit limit-2">{{ props.product.name }}</h3></NuxtLink>
+                <div class="flex justify-between items-center">
+                <span class="text-sm font-normal">{{ props.product.category }}</span>
+                <span class="text-sm font-normal">${{ props.product.price }}</span>
+                </div>
+            <div :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px] bg-red-600 
+            shadow-xl rounded-full flex justify-center items-center hover:bg-white
+            hover:text-red-600 font-bold transition duration-300`">
+            <i class="ri-shopping-cart-2-line">Hot!</i>
+            </div>
+        </div>
+    </section>
+</template>
