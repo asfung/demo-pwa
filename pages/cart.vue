55d7097111a03884ce1c27f59a465f084e23843a
straufer <paungcuy15@gmail.com>
2023-10-28 20:19:09 +0700

    category, cart feature

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,19 +1,86 @@
-<script setup>
-    import Header from '../components/Header.vue'
-    import Footer from '../components/Footer.vue'
-    document.title = "Cart"
+<script lang="ts" setup>
+import { ProductsType } from "~/types/ProductsType";
+import navbar from '~/components/Header.vue';
+import footer from '~/components/Footer.vue'
+
+const products = ref<ProductsType[]>([]);
+const totalPrice = computed(() => {
+  return products.value
+    .filter((product) => product.price !== undefined)
+    .reduce(
+      (accumulator, currentValue) => accumulator + currentValue.price!,
+      0
+    );
+});
+
+onMounted(() => {
+  let localStorageData = localStorage.getItem("products");
+  if (localStorageData) {
+    products.value = JSON.parse(localStorageData);
+  }
+});
+
+const removeCart = (id: number) => {
+    products.value = products.value.filter((item) => item.id !== id);
+    localStorage.setItem("products", JSON.stringify(products.value));
+}
 </script>
 
 <template>
-    <div class="header">
-        <Header />
-    </div>
-    <div class="content">
-        <div>
-            <h1 class="text-4xl">Ini Halaman Cart</h1>
+    <navbar />
+
+  <section>
+    <div class="container">
+      <div class="py-10 flex gap-6">
+        <div class="w-[70%]">
+          <div
+            class="flex justify-between items-center pb-7 border-b border- gray-300 mb-6"
+          >
+            <h1 class="text-3xl font-medium">Shopping Cart</h1>
+            <p class="text-3xl font-medium">{{ products.length }} Items</p>
+          </div>
+          <div v-if="products.length > 0" class="flex flex-col gap-6">
+            <template v-for="(item, index) in products" :key="index">
+            <CardsCardCart :product="item" @removeCart="removeCart"/>
+            </template>
+            </div>
+          <div v-else>
+            <h5 class="text-xl font-light text-center">Cart is empty</h5>
+          </div>
         </div>
+        <div class="w-[30%] bg-white shadow-xl h-max p-6">
+          <h3 class="text-xl font-medium mb-6">Order Summary</h3>
+          <div class="flex flex-col gap-3 border-b border-gray-300 pb-4">
+            <div v-if="products.length > 0">
+              <div
+                v-for="(item, index) in products"
+                :key="index"
+                class="flex gap-4 items-center"
+              >
+                <span class="text-limit limit-1 text-sm">{{ item.name }}</span>
+                <span class="text-sm font-semibold">${{ item.price }}</span>
+              </div>
+            </div>
+            <div v-else>
+              <p class="text-sm text-center font-light">
+                There are no to orders yet
+              </p>
+            </div>
+          </div>
+          <div class="pt-4 flex items-center justify-between mb-6">
+            <span class="text-base">Total</span>
+            <span class="text-base font-bold">${{ totalPrice }}</span>
+          </div>
+          <button
+            class="bg-blue-600 text-white text-base font-bold w-full py-2 rounded-lg"
+          >
+            Checkout
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="footer">
-        <Footer />
-    </div>
+  </section>
+
+  <footer />
+
 </template>
---
+++ pages/category/create.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import navbar from "~/components/Header.vue";
+  import footer from "~/components/Footer.vue";
+</script>
+
+<template>
+  <navbar />
+  <section>
+    <div class="container pt-7">
+      <NuxtLink to="/product" class="text-4xl ml-52"><button class="underline">Back</button></NuxtLink>
+      <FormsCategoryForm />
+    </div>
+  </section>
+  <footer />
+</template>
+
