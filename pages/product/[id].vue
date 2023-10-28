c407461002776b86c695d96f0a5bf1ee0a6eef40
straufer <paungcuy15@gmail.com>
2023-10-28 21:48:54 +0700

    update cart, new category feature and optimize detailproduct

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-    import { products } from "~/composables/constants/products";
-    import { ProductsType } from "~/types/ProductsType";
-const route = useRoute();
-const id =
-  typeof route.params.id === "string" ? parseInt(route.params.id) : null;
-const product = ref(products.find((item: ProductsType) => item.id === id));
+  import { products } from "~/composables/constants/products";
+  import { ProductsType } from "~/types/ProductsType";
 
-document.title = `${product.value?.name} | ${product.value?.description}`;
+  const route = useRoute();
+  const id = typeof route.params.id === "string" ? parseInt(route.params.id) : null;
+  const product = ref(products.find((item: ProductsType) => item.id === id));
+
+  document.title = `${product.value?.name} | ${product.value?.description}`;
 
 </script>
 
