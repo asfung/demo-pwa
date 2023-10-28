d696aa4101794a679e8a2cfbd82bbab2edb96fb3
straufer <paungcuy15@gmail.com>
2023-10-28 21:23:24 +0700

    updated card and category

--- components/DetailProduct.vue
+++ components/DetailProduct.vue
@@ -54,7 +54,7 @@ const addCartStyle = computed(() => {
           <p class="mb-10">{{ props.product.description }}</p>
           <div class="flex flex-col gap-4">
             <div
-              class="w-full flex items-center gap-2 bg-blue-600 text-white py- 3 justify-center rounded-lg cursor-pointer hover:bg-blue-600/80 transition duration-300 relative h-11 font-bold"
+              class="w-full flex items-center gap-2 bg-slate-700 text-white py- 3 justify-center rounded-lg cursor-pointer hover:bg-slate-600/80 transition duration-300 relative h-11 font-bold"
               :style="addCartStyle"
               >
               <i class="ri-shopping-cart-2-line"></i>
