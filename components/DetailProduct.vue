8afe997057ab123b2bfeb9b9510cdd7af17ae421
straufer <paungcuy15@gmail.com>
2023-10-29 00:53:34 +0700

    notification and change effect

--- components/DetailProduct.vue
+++ components/DetailProduct.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 import { ProductsType } from "~/types/ProductsType";
 
 const props = defineProps({
@@ -9,44 +8,69 @@ const props = defineProps({
   },
 });
 
-  const oneProduct = ref(props.product);
-  const isShowAlert = ref(false);
+const oneProduct = ref(props.product);
+const isShowAlert = ref(false);
+const isAddedToCart = ref(false);
 
-  const addCart = () => {
+const addCart = () => {
+  // oneProduct.value.isCart = !oneProduct.value.isCart;
+  // let localStorageData = localStorage.getItem("products");
+  // let productOfCart: ProductsType[] = [];
+  // if (localStorageData) {
+  //   productOfCart = JSON.parse(localStorageData);
+  // }
+  // if (oneProduct.value.isCart) {
+  //   productOfCart.push(oneProduct.value);
+  //   localStorage.setItem("products", JSON.stringify(productOfCart));
+  //   isShowAlert.value = true;
+  //   const form = ref({
+  //     name: "",
+  //   });
+  //   form.value.name = "";
+  //   setTimeout(() => {
+  //     isShowAlert.value = false;
+  //   }, 8000);
+  //   console.log("masuk ke keranjang.....")
+  // } else {
+  //   productOfCart = productOfCart.filter(
+  //     (item) => item.id !== oneProduct.value.id
+  //   );
+  //   localStorage.setItem("products", JSON.stringify(productOfCart));
+  //   isShowAlert.value = false;
+  //   const form = ref({
+  //     name: "",
+  //   });
+  //   form.value.name = "";
+  //   setTimeout(() => {
+  //     isShowAlert.value = true;
+  //   }, 8000);
+  //   console.log("keluar dari keranjang....")
+  // }
+
   oneProduct.value.isCart = !oneProduct.value.isCart;
   let localStorageData = localStorage.getItem("products");
   let productOfCart: ProductsType[] = [];
+
   if (localStorageData) {
     productOfCart = JSON.parse(localStorageData);
   }
+
   if (oneProduct.value.isCart) {
     productOfCart.push(oneProduct.value);
-    localStorage.setItem("products", JSON.stringify(productOfCart));
-    isShowAlert.value = true;
-    const form = ref({
-      name: "",
-    });
-    form.value.name = "";
-    setTimeout(() => {
-      isShowAlert.value = false;
-    }, 8000);
-    console.log("masuk ke keranjang.....")
+    isAddedToCart.value = true;
   } else {
     productOfCart = productOfCart.filter(
       (item) => item.id !== oneProduct.value.id
     );
-    localStorage.setItem("products", JSON.stringify(productOfCart));
-    isShowAlert.value = false;
-    const form = ref({
-      name: "",
-    });
-    form.value.name = "";
-    setTimeout(() => {
-      isShowAlert.value = true;
-    }, 8000);
-    console.log("keluar dari keranjang....")
+    isAddedToCart.value = false;
   }
-  
+
+  localStorage.setItem("products", JSON.stringify(productOfCart));
+  isShowAlert.value = true;
+
+  setTimeout(() => {
+    isShowAlert.value = false;
+  }, 6000);
 };
 
 </script>
@@ -56,26 +80,31 @@ const props = defineProps({
     <div class="container">
       <NuxtLink
         to="/product"
-        class="bg-white border border-slate-300 w-max flex items-center gap-1 py-2 px-5 rounded-full mb-7 cursor-pointer">
+        class="bg-white border border-slate-300 w-max flex items-center gap-1 py-2 px-5 rounded-full mb-7 cursor-pointer"
+      >
         <i class="ri-arrow-left-s-line text-base font-medium"></i>
         <span class="text-base font-medium">Kembali</span>
       </NuxtLink>
       <div
         v-if="isShowAlert"
-        class="p-4 mb-4 text-sm rounded-lg bg-green-100 text-green-800">
-        Successfully Adding To Cart
-      </div>
-      <div
-        v-else-if="!isShowAlert"
-        class="p-4 mb-4 text-sm rounded-lg bg-green-100 text-green-800">
-        Removed From Cart 
+        class="p-4 mb-4 text-sm rounded-lg"
+        :class="{
+          'bg-green-100 text-green-800': isAddedToCart,
+          'bg-red-100 text-red-800': !isAddedToCart,
+        }"
+      >
+        {{
+          isAddedToCart ? "Successfully Adding To Cart" : "Removed From Cart"
+        }}
       </div>
       <div class="flex items-center">
         <div
-          class="w-1/2 bg-gray-300 mr-5 rounded-3xl flex justify-center items- center p-5 h-[500px]">
+          class="w-1/2 bg-gray-300 mr-5 rounded-3xl flex justify-center items- center p-5 h-[500px]"
+        >
           <img
             :src="props.product.image"
-            class="w-full h-full object-contain"/>
+            class="w-full h-full object-contain"
+          />
         </div>
         <div class="w-1/2 pl-5">
           <p class="text-xl font-light mb-3">{{ props.product.category }}</p>
@@ -84,11 +113,14 @@ const props = defineProps({
           <p class="mb-10">{{ props.product.description }}</p>
           <div class="flex flex-col gap-4">
             <div
-              class="w-full flex items-center gap-2 bg-slate-700 text-white py- 3 justify-center rounded-lg cursor-pointer hover:bg-slate-600/80 transition duration-300 relative h-11 font-bold"
+              class="w-full flex items-center gap-2 py-3 justify-center rounded-lg cursor-pointer hover:bg-slate-600/80 transition duration-300 relative h-11 font-bold"
               @click="addCart"
-              >
+              :class="{
+                'bg-slate-700 text-white': !isAddedToCart,
+                'bg-white text-red-500 border border-red-500 hover:text-white hover:border-none hover:!bg-red-500': isAddedToCart,
+              }">
               <i class="ri-shopping-cart-2-line"></i>
-              <span>Add to Cart</span>
+              <span>{{ isAddedToCart ? "Remove From Cart" : "Add to Cart" }}</span>
             </div>
           </div>
         </div>
