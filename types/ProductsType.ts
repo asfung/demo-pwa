b71791a54efe9710969c184b701a89d4f727bf1c
straufer <paungcuy15@gmail.com>
2023-11-12 14:51:17 +0700

    adding form interface for create product

--- types/ProductsType.ts
+++ types/ProductsType.ts
@@ -6,3 +6,14 @@ export interface ProductsType {
     image? : string,
     description? : string
 }
+
+export interface FormField {
+    id?: string;
+    name?: string;
+    label?: string;
+    type?: string;
+    value?: string | object;
+    placeholder?: string;
+    checked?: boolean;
+    required?: boolean;
+}
