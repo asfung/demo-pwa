d573d25265a233682dcf216a0fdd5e14456c6832
straufer <paungcuy15@gmail.com>
2023-11-12 14:49:48 +0700

    update create product to database and list the data from supabase

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
   import { products } from "~/composables/constants/products";
   import { ProductsType } from "~/types/ProductsType";
+  import { useProductsStore } from "~/stores/products";
 
   const route = useRoute();
   const id = typeof route.params.id === "string" ? parseInt(route.params.id) : null;
+
   const product = ref(products.find((item: ProductsType) => item.id === id));
 
   document.title = `${product.value?.name} | ${product.value?.description}`;
+
+  const findProduct = ref(useProductsStore)
 
 </script>
 
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,18 +1,33 @@
 <script lang="ts" setup>
-  import { products } from '~/composables/constants/products';
-  import Header from '~/components/Header.vue'
-  import Footer from '~/components/Footer.vue'
+  import { useProductsStore } from "~/stores/products";
 
-  document.title = "Products"
+  const productStore = useProductsStore();
+  const allProducts = ref([]);
 
-  // const allProducts = ref(products);
-  const selectedCategory = ref("");
-    const allProducts = computed(() => {
-    if (selectedCategory.value) {
-    return products.filter((item) => item.category === selectedCategory.value);
-    }
-    return products;
+  productStore.getAllProducts().then(() => {
+    allProducts.value = productStore.products;
   });
+
+  const selectedCategory = ref("");
+
+  const categoryStore = useCategoryStore();
+  const { categories } = storeToRefs(categoryStore);
+  categoryStore.getAllCategory();
+
+  const sortListProduct = computed(() => {
+  if (!selectedCategory.value) {
+    return allProducts.value; 
+  } else {
+    // ignore the fkin error here !
+    return allProducts.value.filter(product => product.category === selectedCategory.value);
+  }
+});
+
+// TODO 
+// 1. sorting a list product on dropdwn components
+// 2. fix the undefined variable, (problem: get value variable didnt declared scope)
+// 3. detail product showed on path variable by id
+
 
 </script>
 
@@ -24,20 +39,23 @@
   <section>
     <div class="container">
       <div class="py-10">
-      <h1 class="text-4xl font-bold pt-7">Products</h1>
-      <div class="py-10">
-        <div class="mb-6 flex justify-end gap-6">
-          <Dropdown @selected-category="selectedCategory = $event" />
-          <NuxtLink to="/category/create" class="bg-slate-700 text-white flex justify-
-          center items-center px-3 rounded-lg hover:bg-slate-500">Create New Category</NuxtLink>
-        </div>
-        <div class="flex gap-6 flex-wrap mx-auto">
-          <template v-for="(item, index) in allProducts" :key="index">
-              <CardsCardProduct :product="item" class="w-[calc(100%/4-18px)]"/>
-          </template>
+        <h1 class="text-4xl font-bold pt-7">Products</h1>
+        <div class="py-10">
+          <div class="mb-6 flex justify-end gap-6">
+            <Dropdown @selected-category="selectedCategory = $event" />
+            <NuxtLink to="/category/create"
+              class="bg-slate-700 text-white flex justify- center items-center px-3 rounded-lg hover:bg-slate-500">Create
+              New Category</NuxtLink>
+            <NuxtLink to="/product/create"
+              class="bg-green-200 text-green-700 flex justify-center items-center px-3 rounded-lg border border-green-700 hover:bg-green-500 hover:text-white">Create Products</NuxtLink>
+          </div>
+          <div class="flex gap-6 flex-wrap mx-auto">
+            <template v-for="(item, index) in sortListProduct" :key="index">
+              <CardsCardProduct :product="item" class="w-[calc(100%/4-18px)]" />
+            </template>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   </section>
 
